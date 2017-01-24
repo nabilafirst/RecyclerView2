deaf293ff7717d0af1eceec8ff5dcc89b0c0d837
nabilafirst <nabilafirsta@gmail.com>
2017-01-24 15:28:25 +0700

    RecyclerView List

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview2/model/Hotel.java
@@ -0,0 +1,14 @@
+package model;
+
+import android.graphics.drawable.Drawable;
+
+public class Hotel {
+    public String judul, deskripsi;
+    public Drawable foto;
+
+    public Hotel(String judul, String deskripsi, Drawable foto) {
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+    }
+}
